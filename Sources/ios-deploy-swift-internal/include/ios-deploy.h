8e3a516594cae68a256b212d2375732b8dac3579
Joe Hinkle <joehinkle11@gmail.com>
2022-05-03 19:53:22 -0500

    swift calls object c code

--- Sources/ios-deploy-swift-internal/include/ios-deploy.h
+++ Sources/ios-deploy-swift-internal/include/ios-deploy.h
@@ -5,4 +5,4 @@
 //  Created by Joseph Hinkle on 5/3/22.
 //
 
-int ios_deploy_main(int argc, char *argv[])l
+int ios_deploy_main(int argc, char *argv[]);
--- Sources/ios-deploy-swift/ios_deploy_swift.swift
+++ Sources/ios-deploy-swift/ios_deploy_swift.swift
@@ -1,7 +1,16 @@
 
 import ios_deploy_swift_internal
+import Foundation
 
+// Converting Swift strings to c string from https://stackoverflow.com/a/29469618/3902590
 
-public func asdf() {
-    let ok = ios_deploy_swift_internal.ios_deploy_main()
+public func ios_deploy_swift(args: [String]) -> CInt {
+    let argc: CInt = CInt(args.count)
+    // Create [UnsafeMutablePointer<Int8>]:
+    var cargs = args.map { strdup($0) }
+    // Call C function:
+    let result: CInt = ios_deploy_swift_internal.ios_deploy_main(argc, &cargs)
+    // Free the duplicated strings:
+    for ptr in cargs { free(ptr) }
+    return result
 }
